57653d475e7891040019a41513e78cd002d9a485
Nouman-Gul <Nouman@oasislivestock.com>
2026-02-02 19:16:09 +0500

    added products in vc

--- Zabehaty Shop/ViewController.swift
+++ Zabehaty Shop/ViewController.swift
@@ -6,6 +6,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         Task {
+            await ProductsProvider.viewModel.loadProducts()
             await ProductsProvider.viewModel.loadProduct(by: 1)
         }
         // Do any additional setup after loading the view.
